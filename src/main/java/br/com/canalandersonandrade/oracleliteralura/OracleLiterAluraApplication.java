c2202a8d58bc3a711541ed9ea3f31f78011ea4cb
anderson-andrade-dev <147995158+anderson-andrade-dev@users.noreply.github.com>
2024-11-02 18:38:57 -0300

    Inicio de Projeto.

---
+++ src/main/java/br/com/canalandersonandrade/oracleliteralura/OracleLiterAluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.canalandersonandrade.oracleliteralura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleLiterAluraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleLiterAluraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/canalandersonandrade/oracleliteralura/OracleLiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.canalandersonandrade.oracleliteralura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleLiterAluraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
